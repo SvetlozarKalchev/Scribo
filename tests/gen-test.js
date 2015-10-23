1d237afc3e18ae8e1d69ec5b13da6f9802fac14d
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-23 23:47:37 +0200

    Add general test cases

--- tests/gen-test.js
+++ tests/gen-test.js
@@ -2,3 +2,27 @@
 let Chai = require('chai');
 let expect = Chai.expect;
 let should = Chai.should();
+
+describe('check directory for needed files', () => {
+  it('/ should have app.js', () => {
+    should.exist(require('../app.js'));
+  });
+
+  it('/lib/controllers should have LoggerController.js', () => {
+    should.exist(require('../lib/controllers/LoggerController.js'));
+  });
+
+  it('/lib/models should have Benchmarker.js', () => {
+    should.exist(require('../lib/models/Benchmarker.js'));
+  });
+
+  it('/lib/models should have FileWriter.js', () => {
+    should.exist(require('../lib/models/FileWriter.js'));
+  });
+
+  it('/lib/models should have Logger.js', () => {
+    should.exist(require('../lib/models/Logger.js'));
+  });
+
+
+})
