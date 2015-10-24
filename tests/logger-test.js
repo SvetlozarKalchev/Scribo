241ee3f8f489fee4ce08ed49e847e88dd6ab117e
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-24 23:48:31 +0200

    Add test

--- tests/logger-test.js
+++ tests/logger-test.js
@@ -5,14 +5,26 @@ let expect = Chai.expect;
 let should = Chai.should();
 
 describe('class Logger', () => {
-  it('class should be declared', () => {
+  it('should be declared', () => {
     should.exist(require('../lib/models/Logger.js'));
   });
 
   it('should have a method logEvent', () => {
     let Logger = require('../lib/models/Logger.js');
-    let eventName = 'Test Event'
 
     assert.isFunction(Logger.logEvent, 'logEvent() is function');
   });
-})
+});
+
+describe('.logEvent()', () => {
+  it('should return no error and result true if args are correct', () => {
+    let Logger = require('../lib/models/Logger.js');
+    let eventName = 'Test Event';
+
+    Logger.logEvent(eventName, (err, res) => {
+      console.log(err);
+      //assert(err).is.null;
+      done();
+    });
+  });
+});
