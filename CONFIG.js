66cf31da7445ba2dd27b4e6d817657002788deb9
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-25 12:25:19 +0100

    Add FileWriter logic

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+'use strict'
+/*
+  Set events and the corresponding files here
+*/
+let filePath = './logs/';
+let events = new Map();
+
+events.set('GET', filePath + 'get-log.txt');
+events.set('POST', filePath + 'post-log.txt');
+events.set('DB Write', filePath + 'db-log.txt');
+
+module.exports = events;
