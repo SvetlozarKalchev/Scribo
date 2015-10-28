7b4223b34a280410722292f659d17287f2aa2bb0
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-28 15:32:55 +0100

    Make callback optional in Logger module

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let Logger = require('./lib/models/Logger.js');
 let Benchmarker = require('./lib/models/Benchmarker.js');
 
 module.exports = {
-  Logger: Logger,
   logEvent: Logger.logEvent.bind(Logger),
   startBench: Benchmarker.startBench.bind(Benchmarker),
   endBench: Benchmarker.endBench.bind(Benchmarker)
--- tests/benchmarker-test.js
+++ tests/benchmarker-test.js
@@ -5,3 +5,30 @@ let expect = Chai.expect;
 let should = Chai.should();
 
 let Benchmarker = require('../lib/models/Benchmarker.js');
+
+// Arguments to plug in the class methods.
+
+describe('class Benchmarker', () => {
+
+  describe('.toMS()', () => {
+    it('should return no error and int result when args are correct', (done) => {
+      Benchmarker.toMs(10, 123456, (err, result) => {
+        expect(err).to.be.a('null');
+        expect(result).to.be.a('string');
+      });
+      done();
+    });
+  });
+
+  describe('.startBench()', () => {
+    it('should return no error and an array when args are correct', (done) => {
+      Benchmarker.startBench('Test Bench', (err, result) => {
+        expect(err).to.be.a('null');
+        expect(result).to.be.an('array');
+      });
+      done();
+    });
+  });
+
+
+});
