0389ffd6cf9d3301bda58795c333b2403374357b
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-29 21:29:33 +0100

    Clean up and add instructions

--- EVENTS.js
+++ EVENTS.js
@@ -1,13 +1,15 @@
 'use strict'
 /*
-  Set events and the corresponding files here
+  Set events and their corresponding files here.
+
+  Here's an example of how to set events:
+
+  events.set('GET', filePath + 'get-log.txt');
+  events.set('POST', filePath + 'post-log.txt');
+  events.set('DB Write', filePath + 'db-log.txt');
 */
+
 let filePath = './logs/';
 let events = new Map();
-
-events.set('GET', filePath + 'get-log.txt');
-events.set('POST', filePath + 'post-log.txt');
-events.set('DB Write', filePath + 'db-log.txt');
-events.set('kur', filePath)
 
 module.exports = events;
